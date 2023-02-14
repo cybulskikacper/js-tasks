290b72f7d3a635c7abfeca51d2404951e7c0d825
Kacper Cybulski <kacpercybulski@macbook-air-kacper.home>
2023-02-14 14:34:11 +0100

    added everything

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const numbers = [5,8,10,23,48,60]
 
-for(const number of numbers) { 
-    console.log(number / 5);
-}
-console.log('-----');
 
-for(const number of numbers) { 
-    if(number %2 === 0) { 
-        console.log(`%cLiczba ${number} jest parzysta`,'background-color:gold;font-size:16px');
-    } else { 
-        console.log(`%cLiczba ${number} nie jest parzysta`, 'background-color:tomato; font-size:16px');
-    }
-}
+const ul = document.createElement
